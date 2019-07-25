44dbd2a46fb246ec513d666c453d4ce0f0d1aa0a
Darwin Sodhi <darwin193@live.com>
2019-07-25 10:15:00 -0700

    new figure

--- analysis/Darwin_phylogeny.R
+++ analysis/Darwin_phylogeny.R
@@ -202,3 +202,13 @@ mpd.z<-as.data.frame(rbind(single.sp, all.genus), stringsAsFactors=FALSE)
 pdf("Mean pairwise distances.pdf")
 boxplot(as.numeric(V2) ~ as.factor(V1), data=mpd.z, ylab = "SES.MPD", main = "Mean pairwise distances between hosts")
 dev.off()
+
+
+
+single.sp<-cbind(rep("single.species", length(mntd.single.sp.in.genus$mntd.obs.z)),mntd.single.sp.in.genus$mpd.obs.z)
+all.genus<-cbind(rep("all.genus", length(mntd.all.sp.in.genus$mntd.obs.z)),mntd.all.sp.in.genus$mpd.obs.z)
+mntd.z<-as.data.frame(rbind(single.sp, all.genus), stringsAsFactors=FALSE)
+
+pdf("Mean pairwise distances.pdf")
+boxplot(as.numeric(V2) ~ as.factor(V1), data=mpd.z, ylab = "SES.MPD", main = "Mean pairwise distances between hosts")
+dev.off()
